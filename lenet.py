e6d2dfa26986fc3b2a235f204079d4cf29bfd2ce
wangx404 <wangx404@gmail.com>
2019-03-28 14:43:20 +0800

    Update lenet.py

--- lenet.py
+++ lenet.py
@@ -1,7 +1,10 @@
 from mxnet import gluon
 
-
 class LeNet(gluon.nn.HybridBlock):
+    """
+    modify LeNet, and use it to output feature and class prediction at the same time.
+    network structure: conv(5, 20), maxpool, conv(5, 50), maxpool, flatten, dense, denses.
+    """
     def __init__(self, classes=10, feature_size=2, **kwargs):
         super(LeNet, self).__init__(**kwargs)
         with self.name_scope():
@@ -23,6 +26,10 @@ class LeNet(gluon.nn.HybridBlock):
 
 
 def _make_conv_block(block_index, num_chan=32, num_layer=2, stride=1, pad=2):
+    """
+    Construct a CNN module using several (conv + activation) layers and one maxpooling layer.
+    构建一个由(卷积，激活)×n和最大池化组成的卷积模块。
+    """
     out = gluon.nn.HybridSequential(prefix='block_%d_' % block_index)
     with out.name_scope():
         for _ in range(num_layer):
@@ -34,21 +41,25 @@ def _make_conv_block(block_index, num_chan=32, num_layer=2, stride=1, pad=2):
 
 
 class LeNetPlus(gluon.nn.HybridBlock):
-
+    """
+    Enhanced Lenet.
+    加强版的LeNet。
+    """
     def __init__(self, classes=10, feature_size=2, **kwargs):
         super(LeNetPlus, self).__init__(**kwargs)
-        num_chans = [32, 64, 128]
+        num_channels = [32, 64, 128]
         with self.name_scope():
+            # construct feature extractor
             self.features = gluon.nn.HybridSequential(prefix='')
-
-            for i, num_chan in enumerate(num_chans):
-                self.features.add(_make_conv_block(i, num_chan=num_chan))
-
-            self.features.add(gluon.nn.Dense(feature_size))
+            for i, num_channel in enumerate(num_channels):
+                self.features.add(_make_conv_block(i, num_chan=num_channel))
+            
             self.features.add(gluon.nn.Flatten())
+            self.features.add(gluon.nn.Dense(feature_size))
+            # construct output layer
             self.output = gluon.nn.Dense(classes)
-
+    
     def hybrid_forward(self, F, x, *args, **kwargs):
-        ft = self.features(x)
-        output = self.output(ft)
-        return output, ft
+        feature = self.features(x)
+        output = self.output(feature)
+        return output, feature
